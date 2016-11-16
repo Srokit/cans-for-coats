25d2b6420702570b4f301e9640d628591d64c5d8
srokit <srok35@gmail.com>
2016-11-15 20:57:57 -0500

    serving ang app to index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var app = express();
 
 var config = require('./config');
 
+//Serve up angular app to root route
+app.use(express.static(__dirname + '/angapp'));
+
 app.listen(config.port);
 
 console.log("Listening on port: " + config.port);
