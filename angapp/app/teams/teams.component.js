996d234cb64bbc9f59540f7a401cc1db1be12dc4
srokit <srok35@gmail.com>
2016-11-16 08:51:05 -0500

    Little example with static teams list.

--- angapp/app/teams/teams.component.js
+++ angapp/app/teams/teams.component.js
@@ -1,5 +1,8 @@
 angular.module('app').controller('teamsController', function($scope) {
-
+    $scope.teams = [{
+        imgSrc: "exTeamProfPic.png",
+        name: "ExampleTeam"
+    }]
 });
 
 angular.module('app').component('teams', {
