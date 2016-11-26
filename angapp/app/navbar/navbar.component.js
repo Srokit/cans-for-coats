c2042ec76b088a63caf48fdc39b3606a24e7c8bd
srokit <srok35@gmail.com>
2016-11-26 16:47:38 -0500

    Finalized the component and view files. Set up the index.html file to boot.

--- angapp/app/navbar/navbar.component.js
+++ angapp/app/navbar/navbar.component.js
@@ -1,9 +1,9 @@
 angular.module('app').controller('NavbarController', function($scope) {
-    
+
 });
 
 angular.module('app').component('navbar', {
 
     controller: 'NavbarController',
-    template: 'app/navbar/navbar.view.html'
+    templateUrl: 'app/navbar/navbar.view.html'
 });
