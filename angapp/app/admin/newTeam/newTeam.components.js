b78289a1f9ea18f97bdc2138be0f1fd35c6bc8a7
srokit <srok35@gmail.com>
2016-11-26 18:50:49 -0500

    Filled newTeam component with component skeleton. Added admin/newteam as an angular route.

---
+++ angapp/app/admin/newTeam/newTeam.components.js
@@ -0,0 +1,9 @@
+angular.module('app').controller('NewTeamController', function($scope) {
+
+});
+
+angular.module('app').component('newTeam', {
+
+    controller: 'NewTeamController',
+    templateUrl: 'app/admin/newTeam/newTeam.view.html'
+});
--- angapp/app/app.config.js
+++ angapp/app/app.config.js
@@ -2,10 +2,13 @@ angular.module('app').config(function($routeProvider) {
 
     $routeProvider
     .when('/', {
-        template: '<teams></teams>',
+        template: '<teams></teams>'
     })
     .when('/teams', {
-        template: '<teams></teams>',
+        template: '<teams></teams>'
+    })
+    .when('/admin/newteam', {
+        template: '<new-team></new-team>'
     })
     .otherwise({
         redirectTo: '/'
