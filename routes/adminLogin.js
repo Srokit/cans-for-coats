81a6d3d5719237cbe13a7919476dacf9883c7ed9
srokit <srok35@gmail.com>
2016-11-26 21:20:48 -0500

    AdminLogin route will now test against static AdminEmail and Password values from config.js object.

--- routes/adminLogin.js
+++ routes/adminLogin.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
+
 var router = express.Router();
+
+var config = require('../config');
 
 router.post('/', function(req, res) {
 
-    
+    var givenEmail = req.body.credentials.email;
+    var givenPass = req.body.credentials.password;
+
+    if(givenEmail === config.adminEmail && givenPass === config.adminPass) {
+
+        var adminToken = jwt.sign({admin: true}, config.jwtSec);
+        res.json({success: true, adminToken: adminToken});
+    }
+    else {
+        res.json({success: false, err: "InvalidCredentials"});
+    }
 });
 
 module.exports = router;
